23e0fab3eb166dbbf6141c396aba7917a589a5b1
hekal74 <eslamhekal74@gmail.com>
2025-05-05 23:59:58 +0300

    deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Authenticated-Blog-Dashboard/',
   plugins: [
     react(),
     tailwindcss(),
