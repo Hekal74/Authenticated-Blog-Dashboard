06eef75987270f32d108659edf139cde17b8c6de
Eslam Hekal <eslam.hekal@vodafone.com>
2025-10-02 16:10:20 +0300

    readiness for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Authenticated-Blog-Dashboard/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  base: '/',
+  plugins: [react(), tailwindcss()],
+});
